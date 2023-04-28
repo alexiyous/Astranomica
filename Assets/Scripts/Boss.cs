40609f4ac4cec1fb9a89c2f793d12d9ff22a8a88
Juan Christopher Young <juanyoung41@gmail.com>
2023-04-28 21:48:57 +0700

    Add Learn
    Penambahan File Learn

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Transform movePoint;
+    public float moveInterval = 3f;
+    public LayerMask stopLayer;
+
+    private Coroutine moveCoroutine;
+    private bool isMovingRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePoint.parent = null;
+        moveCoroutine = StartCoroutine(MoveMovePoint());
+    }
+
+    // Coroutine to move the movePoint every moveInterval seconds
+    private IEnumerator MoveMovePoint()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(moveInterval);
+            // Check if there is an object with the "stopLayer" layer at the new movePoint position
+            Collider2D hit = Physics2D.OverlapCircle(movePoint.position + (isMovingRight ? new Vector3(1f, 0f, 0f) : new Vector3(-1f, 0f, 0f)), 0.1f, stopLayer);
+            if (hit != null)
+            {
+                movePoint.position += new Vector3(0f, -1f, 0f);
+                isMovingRight = !isMovingRight;
+            }
+            else
+            {
+                movePoint.position += isMovingRight ? new Vector3(1f, 0f, 0f) : new Vector3(-1f, 0f, 0f);
+            }
+        }
+    }
+
+    // Stop the coroutine when the enemy is disabled
+    private void OnDisable()
+    {
+        StopCoroutine(moveCoroutine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/EnemyMoveBottom.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoveBottom : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Transform movePoint;
+    public float moveInterval = 3f; 
+    private Coroutine moveCoroutine; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePoint.parent = null;
+        moveCoroutine = StartCoroutine(MoveMovePoint());
+    }
+
+    // Coroutine to move the movePoint every moveInterval seconds
+    private IEnumerator MoveMovePoint()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(moveInterval);
+            movePoint.position += new Vector3(0f, -1f, 0f);
+        }
+    }
+
+    // Stop the coroutine when the enemy is disabled
+    private void OnDisable()
+    {
+        StopCoroutine(moveCoroutine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+    }
+}
+
