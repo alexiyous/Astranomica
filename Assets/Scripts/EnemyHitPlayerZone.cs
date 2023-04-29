f3cb30d2e7bb6c421f64a2c8781f75369078ee61
Juan Christopher Young <juanyoung41@gmail.com>
2023-04-29 22:42:39 +0700

    Added Animation (Belum kelar), dll

--- Assets/Scripts/EnemyHitPlayerZone.cs
+++ Assets/Scripts/EnemyHitPlayerZone.cs
@@ -6,11 +6,13 @@ public class EnemyHitPlayerZone : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        Type1_EnemyHealt enemyHealth = other.GetComponent<Type1_EnemyHealt>();
         if (other.CompareTag("Enemy"))
         {
             HealthManager_v1.playerHealth -= 0.5f;
-            Destroy(other.gameObject);            
+            //Destroy(other.gameObject);
+            enemyHealth.TakeDamage(10f);
+            Physics2D.IgnoreCollision(other, GetComponent<Collider2D>());
             if (HealthManager_v1.playerHealth <= 0)
             {
                 // Player is dead, game over
