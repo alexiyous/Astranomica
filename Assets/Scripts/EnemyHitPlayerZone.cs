9e4b502e3011253c92e057678c616ef6402bbdc5
alexiyous <alexiusrenanta@gmail.com>
2023-04-30 12:35:57 +0700

    fixing layer and collider physics 2d

--- Assets/Scripts/EnemyHitPlayerZone.cs
+++ Assets/Scripts/EnemyHitPlayerZone.cs
@@ -6,11 +6,9 @@ public class EnemyHitPlayerZone : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Type1_EnemyHealt enemyHealth = other.GetComponent<Type1_EnemyHealt>();
         if (other.CompareTag("Enemy"))
         {
             HealthManager_v1.playerHealth -= 1f;
-            enemyHealth.TakeDamage(10f);
             Destroy(other.gameObject);
         }
     }
--- Assets/Scripts/Type1_EnemyHealt.cs
+++ Assets/Scripts/Type1_EnemyHealt.cs
@@ -18,7 +18,7 @@ public class Type1_EnemyHealt : MonoBehaviour
 
     public void Die()
     {
-        ScoreManager.instance.AddScore(pointValue);
+        /*ScoreManager.instance.AddScore(pointValue);*/
         Destroy(gameObject);
     }
 }
