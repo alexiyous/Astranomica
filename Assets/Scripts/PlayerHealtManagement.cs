9b56466240cdc767b77ccc2605e2309938b1bef3
Juan Christopher Young <juanyoung41@gmail.com>
2023-04-30 09:13:15 +0700

    Added Animatiion

--- Assets/Scripts/PlayerHealtManagement.cs
+++ Assets/Scripts/PlayerHealtManagement.cs
@@ -6,11 +6,11 @@ public class PlayerHealtManagement : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Type1_EnemyHealt enemyHealth = other.GetComponent<Type1_EnemyHealt>();
+        //Type1_EnemyHealt enemyHealth = other.GetComponent<Type1_EnemyHealt>();
         if (other.CompareTag("Enemy"))
         {
-            //HealthManager_v1.playerHealth -= 1;
-            enemyHealth.TakeDamage(1f);
+            HealthManager_v1.playerHealth -= 1;
+            //enemyHealth.TakeDamage(1f);
             if (HealthManager_v1.playerHealth <= 0)
             {
                 //Player is dead, game over
